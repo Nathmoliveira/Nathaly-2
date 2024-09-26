9f4a8c8099d5c406d280c333761b1db1230ddc90
Nathmoliveira <00001108084503sp@al.educacao.sp.gov.br>
2024-09-26 08:50:08 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);  
+ background("white");
+}
+
+function draw() {
+  stroke("pink")
+  fill("purple")
+  
+  if(mouseIsPressed){
+  rect(mouseX,mouseY,10,10)}
+}
